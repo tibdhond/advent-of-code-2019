edafe5648e7fa29a6d9c04bd51d0803065a9ec72
tibdhond <Tibo.DHondt@UGent.be>
2019-12-04 11:37:37 +0100

    day 4

--- day4/day4.py
+++ day4/day4.py
@@ -1,6 +1,30 @@
 def main():
-    with open("input.txt", 'r') as f:
-        inpt = f.readline()
+    i = "177777"
+    count = 0
+    while int(i) <= 683082:
+        if i[5] == "9":
+            j = 4
+            while i[j] == "9":
+                j -= 1
+            i = "%s%s%s" % (i[:j], str(int(i[j]) + 1), "".join([str(int(i[j]) + 1) for _ in range(5-j)]))
+            if int(i) > 683082:
+                break
+        else:
+            i = i[:-1] + str(int(i[-1]) + 1)
+        adj = 0
+        j = 0
+        while adj == 0 and j < 5:
+            same = 1
+            while j <= 4 and i[j] == i[j+1]:
+                same += 1
+                j += 1
+            if same == 2:
+                adj += 1
+                count += 1
+                print(i)
+                break
+            j += 1
+    print(count)
 
 
 if __name__ == '__main__':
